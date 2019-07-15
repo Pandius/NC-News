15cc7ab949fecf1de20fde6a09369da4b0033d79
Pawel <pandius@poczta.fm>
2019-07-15 12:26:36 +0100

    created migration schema for comments table

--- db/migrations/20190715114213_comments.js
+++ db/migrations/20190715114213_comments.js
@@ -1,8 +1,23 @@
-
-exports.up = function(knex) {
-  
+exports.up = function(connection) {
+  return (
+    connection.schema.createTable('comments'),
+    function(commentsTable) {
+      commentsTable.increments('comment_id').primary();
+      commentsTable
+        .string('author')
+        .references('users.username')
+        .notNullable();
+      commentsTable
+        .integer('article_id')
+        .references('articles.article_id')
+        .notNullable();
+      commentsTable.integer('votes').defaultTo(0);
+      commentsTable.timestamp('created_at').defaultTo(connection.fn.now());
+      commentsTable.text('body').notNullable();
+    }
+  );
 };
 
-exports.down = function(knex) {
-  
+exports.down = function(connection) {
+  return connection.schema.dropTable('comments');
 };
