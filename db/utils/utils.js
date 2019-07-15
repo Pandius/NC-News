df9f43433c941044eeabcba4c9bb2b718b924568
Pawel <pandius@poczta.fm>
2019-07-15 14:51:44 +0100

    created format data function and tested it

--- db/utils/utils.js
+++ db/utils/utils.js
@@ -1,4 +1,11 @@
-exports.formatDates = list => {};
+exports.formatDate = list => {
+  return list.map(Element => {
+    const newObj = {...Element};
+    const date = new Date(Element.created_at).toLocaleString();
+    newObj.created_at = date;
+    return newObj;
+  });
+};
 
 exports.makeRefObj = list => {};
 
--- spec/utils.spec.js
+++ spec/utils.spec.js
@@ -1,11 +1,108 @@
-const { expect } = require('chai');
-const {
-  formatDates,
-  makeRefObj,
-  formatComments,
-} = require('../db/utils/utils');
+const {expect} = require('chai');
+const {formatDate, makeRefObj, formatComments} = require('../db/utils/utils');
 
-describe('formatDates', () => {});
+describe('formatDate', () => {
+  it('returns a new empty array for passed empty array', () => {
+    expect(formatDate([])).to.eql([]);
+    expect(formatDate([])).to.not.equal([]);
+  });
+  it('returns a single object array with changed dateFormat', () => {
+    expect(
+      formatDate([
+        {
+          title: 'Living in the shadow of a great man',
+          topic: 'mitch',
+          author: 'butter_bridge',
+          body: 'I find this existence challenging',
+          created_at: 1542284514171,
+          votes: 100
+        }
+      ])
+    ).to.eql([
+      {
+        title: 'Living in the shadow of a great man',
+        topic: 'mitch',
+        author: 'butter_bridge',
+        body: 'I find this existence challenging',
+        created_at: '11/15/2018, 12:21:54 PM',
+        votes: 100
+      }
+    ]);
+  });
+  it('returns a  multiple obcjects array with changed dateFormat', () => {
+    expect(
+      formatDate([
+        {
+          title: 'Living in the shadow of a great man',
+          topic: 'mitch',
+          author: 'butter_bridge',
+          body: 'I find this existence challenging',
+          created_at: 1542284514171,
+          votes: 100
+        },
+        {
+          title:
+            "The Rise Of Thinking Machines: How IBM's Watson Takes On The World",
+          topic: 'coding',
+          author: 'jessjelly',
+          body:
+            'Many people know Watson as the IBM-developed cognitive super computer that won the Jeopardy! gameshow in 2011. In truth, Watson is not actually a computer but a set of algorithms and APIs, and since winning TV fame (and a $1 million prize) IBM has put it to use tackling tough problems in every industry from healthcare to finance. Most recently, IBM has announced several new partnerships which aim to take things even further, and put its cognitive capabilities to use solving a whole new range of problems around the world.',
+          created_at: 1500584273256
+        },
+        {
+          title: '22 Amazing open source React projects',
+          topic: 'coding',
+          author: 'happyamy2016',
+          body:
+            'This is a collection of open source apps built with React.JS library. In this observation, we compared nearly 800 projects to pick the top 22. (React Native: 11, React: 11). To evaluate the quality, Mybridge AI considered a variety of factors to determine how useful the projects are for programmers. To give you an idea on the quality, the average number of Github stars from the 22 projects was 1,681.',
+          created_at: 1500659650346
+        },
+        {
+          title: 'Making sense of Redux',
+          topic: 'coding',
+          author: 'jessjelly',
+          body:
+            'When I first started learning React, I remember reading lots of articles about the different technologies associated with it. In particular, this one article stood out. It mentions how confusing the ecosystem is, and how developers often feel they have to know ALL of the ecosystem before using React. And as someone who’s used React daily for the past 8 months or so, I can definitely say that I’m still barely scratching the surface in terms of understanding how the entire ecosystem works! But my time spent using React has given me some insight into when and why it might be appropriate to use another technology — Redux (a variant of the Flux architecture).',
+          created_at: 1514093931240
+        }
+      ])
+    ).to.eql([
+      {
+        title: 'Living in the shadow of a great man',
+        topic: 'mitch',
+        author: 'butter_bridge',
+        body: 'I find this existence challenging',
+        created_at: '11/15/2018, 12:21:54 PM',
+        votes: 100
+      },
+      {
+        title:
+          "The Rise Of Thinking Machines: How IBM's Watson Takes On The World",
+        topic: 'coding',
+        author: 'jessjelly',
+        body:
+          'Many people know Watson as the IBM-developed cognitive super computer that won the Jeopardy! gameshow in 2011. In truth, Watson is not actually a computer but a set of algorithms and APIs, and since winning TV fame (and a $1 million prize) IBM has put it to use tackling tough problems in every industry from healthcare to finance. Most recently, IBM has announced several new partnerships which aim to take things even further, and put its cognitive capabilities to use solving a whole new range of problems around the world.',
+        created_at: '7/20/2017, 9:57:53 PM'
+      },
+      {
+        title: '22 Amazing open source React projects',
+        topic: 'coding',
+        author: 'happyamy2016',
+        body:
+          'This is a collection of open source apps built with React.JS library. In this observation, we compared nearly 800 projects to pick the top 22. (React Native: 11, React: 11). To evaluate the quality, Mybridge AI considered a variety of factors to determine how useful the projects are for programmers. To give you an idea on the quality, the average number of Github stars from the 22 projects was 1,681.',
+        created_at: '7/21/2017, 6:54:10 PM'
+      },
+      {
+        title: 'Making sense of Redux',
+        topic: 'coding',
+        author: 'jessjelly',
+        body:
+          'When I first started learning React, I remember reading lots of articles about the different technologies associated with it. In particular, this one article stood out. It mentions how confusing the ecosystem is, and how developers often feel they have to know ALL of the ecosystem before using React. And as someone who’s used React daily for the past 8 months or so, I can definitely say that I’m still barely scratching the surface in terms of understanding how the entire ecosystem works! But my time spent using React has given me some insight into when and why it might be appropriate to use another technology — Redux (a variant of the Flux architecture).',
+        created_at: '12/24/2017, 5:38:51 AM'
+      }
+    ]);
+  });
+});
 
 describe('makeRefObj', () => {});
 
