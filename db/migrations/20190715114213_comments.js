77188c402d2e19a328d32bed039108207aaa9db2
Pawel <pandius@poczta.fm>
2019-07-15 12:46:21 +0100

    formated comments migrate function

--- db/migrations/20190715114213_comments.js
+++ db/migrations/20190715114213_comments.js
@@ -1,21 +1,18 @@
 exports.up = function(connection) {
-  return (
-    connection.schema.createTable('comments'),
-    function(commentsTable) {
-      commentsTable.increments('comment_id').primary();
-      commentsTable
-        .string('author')
-        .references('users.username')
-        .notNullable();
-      commentsTable
-        .integer('article_id')
-        .references('articles.article_id')
-        .notNullable();
-      commentsTable.integer('votes').defaultTo(0);
-      commentsTable.timestamp('created_at').defaultTo(connection.fn.now());
-      commentsTable.text('body').notNullable();
-    }
-  );
+  return connection.schema.createTable('comments', function(commentsTable) {
+    commentsTable.increments('comment_id').primary();
+    commentsTable
+      .string('author')
+      .references('users.username')
+      .notNullable();
+    commentsTable
+      .integer('article_id')
+      .references('articles.article_id')
+      .notNullable();
+    commentsTable.integer('votes').defaultTo(0);
+    commentsTable.timestamp('created_at').defaultTo(connection.fn.now());
+    commentsTable.text('body').notNullable();
+  });
 };
 
 exports.down = function(connection) {
