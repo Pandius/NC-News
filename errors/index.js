8433c24fc16e295159a1b68f2f20dad1a6df5ece
Pawel <pandius@poczta.fm>
2019-07-17 14:20:34 +0100

    created folder and index file to handle route not found, sql, and method not allowed errors

--- errors/index.js
+++ errors/index.js
@@ -1,3 +1,20 @@
+exports.routeNotFound = (req, res) => {
+  res.status(404).send({msg: 'Page not found'});
+};
+
+exports.handleSQLErrors = (err, req, res, next) => {
+  const errCodes = {
+    '22P02': 'Invalid article id',
+    '23502': 'No data to post!'
+  };
+  if (errCodes[err.code]) {
+    res.status(400).send({msg: errCodes[err.code]});
+  } else next(err);
+};
+exports.MethodNotAllowed = (req, res, next) => {
+  res.status(405).send({msg: 'Method not allowed'});
+};
+
 exports.handleCustomErrors = (err, req, res, next) => {
   if (err.status) {
     res.status(err.status).send({msg: err.msg || 'Something went wrong'});
