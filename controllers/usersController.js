380dac3948be1a631dc79f6a59707d17b3170741
Pawel <pandius@poczta.fm>
2019-07-18 13:38:12 +0100

    refactoring

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -4,11 +4,7 @@ exports.sendUserByUsername = (req, res, next) => {
   const {username} = req.params;
   fetchUserByUsername(username)
     .then(user => {
-      if (!user) {
-        return Promise.reject({status: 404, msg: 'user doesn`t exists'});
-      } else {
-        res.status(200).send({user});
-      }
+      res.status(200).send({user});
     })
     .catch(next);
 };
