7bb55ac54a6d3136a7e1972d64498270afc5bdc1
Pawel <pandius@poczta.fm>
2019-07-18 16:03:39 +0100

    created regex to check username is a valid string - not a number

--- models/usersModel.js
+++ models/usersModel.js
@@ -1,15 +1,19 @@
 const connection = require('../db/connection');
 
 exports.fetchUserByUsername = username => {
-  return connection
-    .first('*')
-    .from('users')
-    .where('username', '=', username)
-    .then(user => {
-      if (!user) {
-        return Promise.reject({status: 404, msg: 'user doesnt exists'});
-      } else {
-        return user;
-      }
-    });
+  if (/^[a-z]+$/i.test(username)) {
+    return connection
+      .first('*')
+      .from('users')
+      .where('username', '=', username)
+      .then(user => {
+        if (!user) {
+          return Promise.reject({status: 404, msg: 'user doesnt exists'});
+        } else {
+          return user;
+        }
+      });
+  } else {
+    return Promise.reject({status: 400, msg: 'Bad request'});
+  }
 };
