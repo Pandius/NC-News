76782267c1bd81e4e6682d0cda715461cbf39e92
Pawel <pandius@poczta.fm>
2019-07-19 14:11:17 +0100

    added onDelete function to delete all comments when delete its article

--- db/migrations/20190715114213_comments.js
+++ db/migrations/20190715114213_comments.js
@@ -8,7 +8,8 @@ exports.up = function(connection) {
     commentsTable
       .integer('article_id')
       .references('articles.article_id')
-      .notNullable();
+      .notNullable()
+      .onDelete('CASCADE');
     commentsTable.integer('votes').defaultTo(0);
     commentsTable.timestamp('created_at').defaultTo(connection.fn.now());
     commentsTable.text('body').notNullable();
